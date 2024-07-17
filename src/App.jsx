662e10cae30c08cd1a28df98da55f13e87e7b7ff
renad <renad@pursuit.org>
2024-07-17 13:06:15 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+// DEPENDENCIES
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// PAGES
+import Edit from "./pages/Edit";
+import FourOFour from "./pages/FourOFour";
+import Home from "./pages/Home";
+import Index from "./pages/Index";
+import New from "./pages/New";
+import Show from "./pages/Show";
+
+// COMPONENTS
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <NavBar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/bagels" element={<Index />} />
+            <Route path="/bagels/new" element={<New />} />
+            <Route path="/bagels/:id" element={<Show />} />
+            <Route path="/bagels/:id/edit" element={<Edit />} />
+            <Route path="*" element={<FourOFour />} />
+          </Routes>
+        </main>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
